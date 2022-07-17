83edb95ba8f4d3ee6ab84422a54332659288d67b
Hrithik Adhikari <hrithik.adhikari@truklay.com>
2022-07-17 12:22:26 +0530

    Added Links on Contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -12,4 +13,7 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  routeTo(url:string){
+    window.location.href = url;
+  }
 }
