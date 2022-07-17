2434b8bb6fbb1d4e37a99d5b551c224fec741950
Hrithik Adhikari <hrithik.adhikari@truklay.com>
2022-07-17 11:03:08 +0530

    Fixed Work page for mobiile

--- src/app/constant/work-data.ts
+++ src/app/constant/work-data.ts
@@ -13,22 +13,22 @@ export interface WorkData {
 
 
 export const WORK_DATA: WorkData[] = [
-    // {
-    //     heading: 'Three.js',
-    //     type: 'Fun',
-    //     isData: false,
-    //     startTime: '',
-    //     endTime: '',
-    //     brief: '',
-    //     descrition: '',
-    //     logoURL: '',
-    // },
+    {
+        heading: 'Three.js',
+        type: 'Fun',
+        isData: false,
+        startTime: '',
+        endTime: '',
+        brief: '',
+        descrition: '',
+        logoURL: '',
+    },
     {
         heading: 'Teknofeet Marketplace System',
         org: 'Teknofeet',
         type: 'Work',
         isData: true,
-        startTime: 'Nov 2021',
+        startTime: 'Nov 21',
         endTime: 'Present',
         brief: `Teknofeet envissioned itself as a marketplace, enabling brands to leverage their technology to reach to customer base and minimize the returns through online sale.
         <br>I designed the architecture, lead the software team which developed the system enabling the brands to seamlessly integrate with us directly or through Aggregators such as OMS Guru, Uniware, BrownTape, etc`,
@@ -40,7 +40,7 @@ export const WORK_DATA: WorkData[] = [
         org: 'Teknofeet',
         type: 'Work',
         isData: true,
-        startTime: 'Nov 2021',
+        startTime: 'Nov 21',
         endTime: 'Present',
         brief: `Teknofeet is first Indian Footcommerce company to talk about fit according to volumental analysis of foot. 
         Teknofeet uses CV and ML to accurately generate a 3-D feet model and then recommends shoe after matching feet dimensions with footwear inner dimensions.
@@ -51,24 +51,29 @@ export const WORK_DATA: WorkData[] = [
     },
     {
         heading: 'Warehouse Management System',
-        org: 'Addverb',
+        org: 'Addverb Technologies',
         type: 'Work',
         isData: true,
-        startTime: 'Jun 2021',
-        endTime: 'Oct 2021',
-        brief: '',
+        startTime: 'Jun 21',
+        endTime: 'Oct 21',
+        brief: `Addverb Technologies is India's only complete solution provider for warehouse automation. Addverb got $132M funding in 2021 from Reliance Industries.
+        <br><br>I was responsible for development of a <span>highly configurable</span></b> Midbound Microservice. 
+        It handles all the operations that happen once the SKU is inside a warehouse and accounted for.
+        `,
         descrition: '',
         logoURL: '',
         tools: 'Angular, Spring, Kafka, SQL, Docker',
     },
     {
-        heading: 'SSO',
-        org: 'Addverb',
+        heading: 'Single Sign On (SSO)',
+        org: 'Addverb Technologies',
         type: 'Internship',
         isData: true,
-        startTime: 'Aug 2020',
-        endTime: 'Jan 2021',
-        brief: '',
+        startTime: 'Aug 20',
+        endTime: 'Jan 21',
+        brief: `Addverb Technologies have many firms with multiple warehouse as their clients such as Pepsico, Flipkart, Reliance, ITC, Asian Paints, etc
+        <br><br>Our team developed SSO which is as configurable as possible to reuse code and provide role based access to the user.
+        We ended up creating a common platform for accessing all our systems at Addverb.`,
         descrition: '',
         logoURL: '',
         tools: 'Angular, Spring, Kafka, SQL, Docker',
